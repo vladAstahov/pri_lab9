676bf368f44365b3b8ea5feede8b50e564bedcf6
Vlad Astahov <avl@burno.tech>
2023-12-24 23:42:30 +0300

    add cin numbers and gitignore

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "std.h"
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
+    int a, b;
+    cout << "Ender first number: ";
+    cin >> a;
+    cout << "Ender second number: ";
+    cin >> b;
+
+    return 0;
 }
